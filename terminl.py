0d59a5b8d7d78db924acabc476aef10a2d029f00
lifeandlevelups <161933390+lifeandlevelups@users.noreply.github.com>
2024-08-09 14:23:33 +0530

    Update terminl.py

--- terminl.py
+++ terminl.py
@@ -1,24 +1,23 @@
 # Prompt user to enter a class method
-classmethod = int(input('Enter cm (1 for addition, 2 for subtraction, 3 for multiplication): '))
-
-# Check the value of classmethod and print the corresponding message
-if classmethod == 1:
-    print("cm set to addition")
-elif classmethod == 2:
-    print("cm set to subtraction")
-elif classmethod == 3:
-    print("cm set to multiplication")
+classmethod = int(input('Enter cm (1 , 2 , 3 ): '))
 
 # Prompt user to enter an operation
 vas = int(input('Enter op (1 for addition, 2 for subtraction, 3 for multiplication): '))
+
+# Check the value of classmethod and print the corresponding message
+if vas == 1:
+    print("op set to addition")
+elif vas == 2:
+    print("op set to subtraction")
+elif vas == 3:
+    print("op set to multiplication")
 
 # Prompt user to enter another class method
 a = int(input('2 Enter cm: '))
 
 # Print the set message and the operation value
-print("set")
+print("set 2nd cm")
 opration = vas
-print(opration)
 
 # Perform the operation based on user input
 if opration == 1:
@@ -32,4 +31,5 @@ elif opration == 3:
     print(f"Result of multiplication: {result}")
 else:
     print("Invalid operation")
+
 
